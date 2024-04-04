1b2be623a9cddc93bf58150d468c7afc6d9b931e
Hakurei Reimu <superh16@outlook.com>
2024-04-04 09:19:34 +0800

    update

--- newindex/assets/js/pagecontrol.js
+++ newindex/assets/js/pagecontrol.js
@@ -1,5 +1,18 @@
+function hideAll()
+{
+    var OG1 = document.getElementById("OfficalGame");
+    OG1.style.display="none";
+    var index1 = document.getElementById("index");
+    index1.style.display="none";
+}
+
 function hideOfficalGame()
 {
     var OG1 = document.getElementById("OfficalGame");
     OG1.style.display="none";
+}
+
+function showOfficalGame(){
+    var OG1 = document.getElementById("OfficalGame");
+    OG1.style.display="";
 }
